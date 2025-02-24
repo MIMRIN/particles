97fb5299cdafef838dedbab40bb87557bc05138e
MIMRIN <1kitkitkit1@gmail.com>
2025-02-24 17:03:10 +0300

    Made code more readable.

--- main1.cpp
+++ main1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define n 100 // number of particles
 #define dt 0.01 // dt
 #define v0 2 // initial velocity
-#define N 100
+#define N 100 // number of iterations
 // defining vectors
 class vec {
 public:
@@ -56,9 +56,9 @@ vec vel(vec r1, vec r3) {
 
 int main()
 {
-    ofstream of("list.xyz");
-    long double K = 0;
-    long double P = 0;
+    ofstream of("list.xyz"); // creating a file
+    long double K = 0; // kinetic energy
+    long double P = 0; // potential energy
     // creating an array for PBC
     vec rrr[26];
     int kkk = 0;
@@ -78,21 +78,21 @@ int main()
     vec A0[n]; // initial accelerations
     vec R1[n]; // 1-step positions
     vec V[n]; // 1 step velocities
-    //
     // random number generator
     random_device rd;
     mt19937 gen(rd());
-    uniform_real_distribution<> dis(0, e);
+    uniform_real_distribution<> dis(0, e); // from 0 to e
     // generating initial positions
     of << n << endl << endl;
-    for(int i = 0; i < n; i++) {
+    // generating initial positions
+    for(int i = 0; i < n; i++) { 
         R0[i].x = dis(gen);
         R0[i].y = dis(gen);
         R0[i].z = dis(gen);
         of << "A" << i << " " << R0[i].x << " " << R0[i].y << " " << R0[i].z << endl;
     }
-    //
-    for(int i = 0; i < n; i++) { // calculating A0
+    // calculating A0
+    for(int i = 0; i < n; i++) { 
         for(int j = 0; j < n && j != i; j++) {
             vec v = R0[i] - R0[j];
             long double r = mod(v);
@@ -112,7 +112,7 @@ int main()
         }
         A0[i] = A0[i] * 24;
     }
-    // находим R1
+    // calculating R1
     of << n << endl << endl;
     for(int i = 0; i < n; i++) {
         long double a = dis(gen) / e * 2 * M_PI;
@@ -123,10 +123,10 @@ int main()
         of << "A" << i << " " << R1[i].x << " " << R1[i].y << " " << R1[i].z << endl;
     }
     //
-    for(int I = 0; I < N; I++) { // совершаем итерации
+    for(int I = 0; I < N; I++) { // iterating
         of << n << endl << endl;
-        vec A[n]; // инициализируем нулевое ускорение
-        for(int i = 0; i < n; i++) { // находим ускорение для следрадвека
+        vec A[n]; // initiating acceleration
+        for(int i = 0; i < n; i++) {
             for(int j = 0; j < n && j != i; j++) {
                 vec v = R1[i] - R1[j];
                 long double r = mod(v);
@@ -139,20 +139,20 @@ int main()
                         long double r1 = mod(v1);
                         if(r1 <= c) {
                             A[i] = A[i] + v1 * (2 - pow(r1, 6)) / pow(r1, 14);
-                            break; // если нашли, то дальше не ищем
+                            break; // if found then no need to continue
                         }
                     }
                 }
             }
             A[i] = A[i] * 24;
         }
-        for(int i = 0; i < n; i++) { // обновляем радвеки
+        for(int i = 0; i < n; i++) { // updating R1, V, R0
             of << "A" << i << " " << R1[i].x << " " << R1[i].y << " " << R1[i].z << endl;
             vec Rt = R1[i];
             R1[i] = pos(R0[i], R1[i], A[i]);
             V[i] = vel(R0[i], R1[i]);
             R0[i] = Rt;
-            // если вышел из коробки, то корректируем данные
+            // if particle escapes then move it to the other side of the box
             if(R1[i].x > e) {
                 R0[i].x = R0[i].x - R1[i].x;
                 R1[i].x = 0;
@@ -180,7 +180,7 @@ int main()
         }
         K = 0;
         P = 0;
-        for(int i = 0; i < n; i++) { //считаем полную энергию
+        for(int i = 0; i < n; i++) { // calculating energy
             K += pow(mod(V[i]), 2);
             for(int j = i+1; j < n; j++) {
                 long double r = mod(R0[i] - R0[j]);
@@ -191,6 +191,6 @@ int main()
         P *= 4;
         cout << K << endl;
     }
-    of.close();
+    of.close(); // closing the file
     return 0;
 }
