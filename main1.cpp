1030adf1a2aee143c54dd9d369a6d6ac2ecf999b
MIMRIN <1kitkitkit1@gmail.com>
2025-03-11 09:05:21 +0300

    Huge update
    Improved calculations and calculation errors fixed.

--- main1.cpp
+++ main1.cpp
@@ -5,16 +5,17 @@ using namespace std;
 #define e 9 // edge of the box: (0; e)x(0; e)x(0; e)
 #define c 3 // cut-off radius
 #define n 216 // number of particles
+#define n3 6
 #define dt 0.001 // dt
 #define v0 2 // initial velocity
-#define N 100000 // number of iterations
+#define N 10000 // number of iterations
 // defining vectors
 class vec {
 public:
-    long double x, y, z;
+    double x, y, z;
     // Конструктор
     vec() : x(0), y(0), z(0) {}
-    vec(long double X, long double Y, long double Z) : x(X), y(Y), z(Z) {}
+    vec(double X, double Y, double Z) : x(X), y(Y), z(Z) {}
     // Перегрузка оператора сложения
     vec operator+(const vec& other) const {
         return vec(x + other.x, y + other.y, z + other.z);
@@ -26,11 +27,11 @@ public:
     }
 
     // Перегрузка оператора умножения
-    vec operator*(const long double& other) const {
+    vec operator*(const double& other) const {
         return vec(x * other, y * other, z * other);
     }
     // Перегрузка оператора деления
-    vec operator/(const long double& other) const {
+    vec operator/(const double& other) const {
         return vec(x / other, y / other, z / other);
     }
     // Перегрузка оператора вывода для удобства
@@ -39,41 +40,24 @@ public:
         return os;
     }
 };
-vec operator*(long double scalar, const vec& v) {
-    return v * scalar; // Используем уже перегруженный оператор vec * long double
+vec operator*(double scalar, const vec& v) {
+    return v * scalar; // Используем уже перегруженный оператор vec * double
 }
 // defining vector functions
 vec pos(vec r0, vec r1, vec a1) { // verlet integration
     return 2*r1-r0+a1*dt*dt;
 }
-long double mod(vec v) { // module
-    return pow(v.x*v.x + v.y*v.y + v.z*v.z, 0.5);
+double mod2(vec v) { // module
+    return v.x*v.x + v.y*v.y + v.z*v.z;
 }
 vec vel(vec r0, vec r2) { // verlet velocities
     return (r2 - r0) / (2 * dt);
 }
 
-
-int main()
-{
+int main() {
     ofstream of("list.xyz"); // creating a file
-    long double K = 0; // kinetic energy
-    long double P = 0; // potential energy
-    // creating an array for PBC
-    vec rrr[26];
-    int kkk = 0;
-    for(int i1 = -1; i1 <= 1; i1++) {
-        for(int i2 = -1; i2 <= 1; i2++) {
-            for(int i3 = -1; i3 <= 1; i3++) {
-                if(i1 != 0 || i2 != 0 || i3 != 0) {
-                    vec rr(i1 * e, i2 * e, i3 * e);
-                    rrr[kkk] = rr;
-                    kkk++;
-                }
-            }
-        }
-    }
-    //
+    double K = 0; // kinetic energy
+    double P = 0; // potential energy
     vec R0[n]; // initial positions
     vec A0[n]; // initial accelerations
     vec R1[n]; // 1-step positions
@@ -81,98 +65,61 @@ int main()
     // random number generator
     random_device rd;
     mt19937 gen(rd());
-    uniform_real_distribution<> dis(0, e); // from 0 to e
+    uniform_real_distribution<> dis(0, 2 * M_PI); // from 0 to 2pi
     // generating initial positions
     of << n << endl << endl;
-    // generating initial positions // do not need random
-    /*
-    for(int i = 0; i < n; i++) { 
-        R0[i].x = dis(gen);
-        R0[i].y = dis(gen);
-        R0[i].z = dis(gen);
-        of << "A" << i << " " << R0[i].x << " " << R0[i].y << " " << R0[i].z << endl;
-    }
-    */
-    for(int i = 1; i <= 6; i++) {
-            for(int j = 1; j <= 6; j++) {
-                for(int k = 1; k <= 6; k++) {
-                    R0[k + (j-1) * 6 + (i-1) * 36 - 1].x = e / 7 * i;
-                    R0[k + (j-1) * 6 + (i-1) * 36 - 1].y = e / 7 * j;
-                    R0[k + (j-1) * 6 + (i-1) * 36 - 1].z = e / 7 * k;
-                    of << "A" << i << " " << R0[i].x << " " << R0[i].y << " " << R0[i].z << endl;
+    for(int i = 1; i <= n3; i++) {
+            for(int j = 1; j <= n3; j++) {
+                for(int k = 1; k <= n3; k++) {
+                    int t = k + (j-1) * n3 + (i-1) * n3 * n3 - 1;
+                    R0[t].x = e / (n3+1) * i;
+                    R0[t].y = e / (n3+1) * j;
+                    R0[t].z = e / (n3+1) * k;
+                    of << "A" << t << " " << R0[t].x << " " << R0[t].y << " " << R0[t].z << endl;
                 }
             }
-        }    
-    // calculating A0
-    for(int i = 0; i < n; i++) { 
-        for(int j = 0; j < n && j != i; j++) {
-            vec v = R0[i] - R0[j];
-            long double r = mod(v);
-            if(r == 0) {
-                of.close();
-                return 1;
-            }
-            if(r <= c) {
-                A0[i] = A0[i] + v * (2 - pow(r, 6)) / pow(r, 14);
-            }
-            else {
-                for(int k = 0; k < 26; k++) {
-                    vec v1 = rrr[k] + v;
-                    long double r1 = mod(v1);
-                    if(r1 == 0) {
-                        of.close();
-                        return 1;
-                    }
-                    if(r1 <= c) {
-                        A0[i] = A0[i] + v1 * (2 - pow(r1, 6)) / pow(r1, 14);
-                        break; // if found then no need to continue
-                    }
+    }    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i != j) {
+                vec v = R0[i] - R0[j];
+                v.x -= e * round(v.x / e);
+                v.y -= e * round(v.y / e);
+                v.z -= e * round(v.z / e);
+                double r2 = mod2(v);
+                if(r2 <= c * c) {
+                    A0[i] = A0[i] + 24 * v * (2 - pow(r2, 3)) / pow(r2, 7);
                 }
             }
         }
-        A0[i] = A0[i] * 24;
     }
     // calculating R1
     of << n << endl << endl;
     for(int i = 0; i < n; i++) {
-        long double a = dis(gen) / e * 2 * M_PI;
-        long double b = dis(gen) / e * 2 * M_PI;
-        R1[i].x = R0[i].x + v0 * cos(a) * cos(b) * dt + 0.5 * dt * dt * A0[i].x;
+        double a = dis(gen);
+        double b = dis(gen);
+        R1[i].x = R0[i].x + v0 * cos(b) * cos(a) * dt + 0.5 * dt * dt * A0[i].x;
         R1[i].y = R0[i].y + v0 * cos(b) * sin(a) * dt + 0.5 * dt * dt * A0[i].y;
         R1[i].z = R0[i].z + v0 * sin(b) * dt + 0.5 * dt * dt * A0[i].z;
         of << "A" << i << " " << R1[i].x << " " << R1[i].y << " " << R1[i].z << endl;
     }
     //
     for(int I = 0; I < N; I++) { // iterating
-        of << n << endl << endl;
+        of << n << endl << I << endl;
         vec A[n]; // initiating acceleration
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n && j != i; j++) {
-                vec v = R1[i] - R1[j];
-                long double r = mod(v);
-                if(r == 0) {
-                    of.close();
-                    return 1;
-                }
-                if(r <= c) {
-                    A[i] = A[i] + v * (2 - pow(r, 6)) / pow(r, 14);
-                }
-                else {
-                    for(int k = 0; k < 26; k++) {
-                        vec v1 = rrr[k] + v;
-                        long double r1 = mod(v1);
-                        if(r1 == 0) {
-                            of.close();
-                            return 1;
-                        }
-                        if(r1 <= c) {
-                            A[i] = A[i] + v1 * (2 - pow(r1, 6)) / pow(r1, 14);
-                            break; // if found then no need to continue
-                        }
+            for(int j = 0; j < n; j++) {
+                if(i != j) {
+                    vec v = R1[i] - R1[j];
+                    v.x -= e * round(v.x / e);
+                    v.y -= e * round(v.y / e);
+                    v.z -= e * round(v.z / e);
+                    double r2 = mod2(v);
+                    if(r2 <= c * c) {
+                        A[i] = A[i] + 24 * v * (2 - pow(r2, 3)) / pow(r2, 7);
                     }
                 }
             }
-            A[i] = A[i] * 24;
         }
         for(int i = 0; i < n; i++) { // updating R1, V, R0
             of << "A" << i << " " << R1[i].x << " " << R1[i].y << " " << R1[i].z << endl;
@@ -181,47 +128,28 @@ int main()
             V[i] = vel(R0[i], R1[i]);
             R0[i] = Rt;
             // if particle escapes then move it to the other side of the box
-            if(R1[i].x > e) {
-                R0[i].x = R0[i].x - R1[i].x;
-                R1[i].x = 0;
-            }
-            else if(R1[i].x < 0) {
-                R0[i].x = R0[i].x - R1[i].x + e;
-                R1[i].x = e;
-            }
-            if(R1[i].y > e) {
-                R0[i].y = R0[i].y - R1[i].y;
-                R1[i].y = 0;
-            }
-            else if(R1[i].y < 0) {
-                R0[i].y = R0[i].y - R1[i].y + e;
-                R1[i].y = e;
-            }    
-            if(R1[i].z > e) {
-                R0[i].z = R0[i].z - R1[i].z;
-                R1[i].z = 0;
-            }
-            else if(R1[i].z < 0) {
-                R0[i].z = R0[i].z - R1[i].z + e;
-                R1[i].z = e;
-            }
-        }
+            R0[i].x -= e * floor(R1[i].x / e);
+            R1[i].x -= e * floor(R1[i].x / e);
+            R0[i].y -= e * floor(R1[i].y / e);
+            R1[i].y -= e * floor(R1[i].y / e);
+            R0[i].z -= e * floor(R1[i].z / e);
+            R1[i].z -= e * floor(R1[i].z / e);
+
+        }         
         K = 0;
         P = 0;
         for(int i = 0; i < n; i++) { // calculating energy
-            K += V[i].x*V[i].x+V[i].y*V[i].y+V[i].z*V[i].z;
+            K += 0.5 * mod2(V[i]);
             for(int j = i+1; j < n; j++) {
-                long double r = mod(R0[i] - R0[j]);
-                if(r == 0) {
-                    of.close();
-                    return 1;
-                }
-                P += (1 - pow(r, 6)) / pow(r, 12);
+                vec v = R0[i] - R0[j];
+                v.x -= e * round(v.x / e);
+                v.y -= e * round(v.y / e);
+                v.z -= e * round(v.z / e);
+                double r2 = mod2(v);
+                P += 4 * (1 - pow(r2, 3)) / pow(r2, 6);
             }
         }
-        K *= 0.5;
-        P *= 4;
-        cout << K+P << " " << K << " " << P << endl;
+        cout << I << " " << K + P << endl;
     }
     of.close(); // closing the file
     return 0;
